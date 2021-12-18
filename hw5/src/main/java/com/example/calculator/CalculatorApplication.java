a6be013bcdd7f0da36fbd394b7a9e4253d1be779
AlsuGibadullina <79659600+AlsuGibadullina@users.noreply.github.com>
2021-12-18 16:07:20 +0300

    hw5

---
+++ hw5/src/main/java/com/example/calculator/CalculatorApplication.java
@@ -0,0 +1,14 @@
+package com.example.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+    public static void main(String[] args) {
+        var calc = new Calculator();
+        calc.calculate(5,"+",255);
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+}
